b456a46fcda81724aef3d55bed30140bd13a335a
rafiearsyad@gmail.com <rafiearsyad@gmail.com>
2023-09-02 10:52:00 +0700

    /

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://wiki.x266.mov',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/codec-wiki/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
