a1c617f60ed6b13240565248688cc6d301701a81
Gianni <giannirosato@proton.me>
2023-09-03 01:39:07 -0400

    social card update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/codec-wiki-social-comp.svg',
       navbar: {
         title: 'Codec Wiki',
         logo: {
