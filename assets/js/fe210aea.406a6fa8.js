72e7ce73efff2855ec1fbacf311edcdf9a1d974e
Gianni <giannirosato@proton.me>
2024-07-30 23:33:23 -0400

    Deploy website - based on ebd1e3694f3ae8413c36ff4338d70fb6a01ed3a2

---
+++ assets/js/fe210aea.406a6fa8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcodec_wiki=self.webpackChunkcodec_wiki||[]).push([[96],{7748:e=>{e.exports=JSON.parse('{"tag":{"label":"web","permalink":"/blog/tags/web","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/web","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
